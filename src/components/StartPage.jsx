8d0c4933021af9ca3f23dc64457bbd6e203f0a5d
Shamil <sham095shamok@mail.ru>
2022-09-08 17:30:28 +0300

    startPage release 1 fixed

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function StartPage() {
-  //   return (
-  //     <div className="StartPage">
-  //       <NavLink className="StartPagelink" to="/registration">Регистрация</NavLink>
-  //       <NavLink className="StartPagelink" to="/login">Авторизация</NavLink>
-  //     </div>
-  //   );
-  // }
-
   return (
     <>
       <div className="top"><h1>Хорошие новости</h1></div>
